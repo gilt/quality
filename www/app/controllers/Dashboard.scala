580cfb0f4126ac830245714c3ecb6bf1a19587c2
Michael Bryzek <michael@gilt.com>
2014-07-11 15:28:35 +0100

    name constants better

--- www/app/controllers/Dashboard.scala
+++ www/app/controllers/Dashboard.scala
@@ -10,12 +10,14 @@ import play.api.Play.current
 object Dashboard extends Controller {
 
   import scala.concurrent.ExecutionContext.Implicits.global
-  private val OneWeekInHours = 24*7
+
+  private val OneDayInHours = 24
+  private val OneWeekInHours = OneDayInHours * 7
 
   def index(page: Int = 0) = Action.async { implicit request =>
     for {
       statisics <- Api.instance.Statistics.get(numberHours = Some(OneWeekInHours))
-      events <- Api.instance.Events.get(numberHours = Some(24), limit = Some(10))
+      events <- Api.instance.Events.get(numberHours = Some(OneDayInHours), limit = Some(10))
     } yield {
       Ok(views.html.dashboard.index(
         statisics.entity,
