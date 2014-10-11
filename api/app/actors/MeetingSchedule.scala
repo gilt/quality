f9a4003ba9f68116b3e56c6cb9a283d94d7e0048
Michael Bryzek <michael@gilt.com>
2014-10-11 12:17:10 -0400

    Implement actual meeting schedule at right date/time

--- api/app/actors/MeetingSchedule.scala
+++ api/app/actors/MeetingSchedule.scala
@@ -1,10 +1,45 @@
 package actors
 
 import com.gilt.quality.models.Organization
-import org.joda.time.DateTime
+import org.joda.time.{DateTime, DateTimeConstants, DateTimeZone}
+
+sealed trait DayOfWeek {
+  def jodaValue: Int
+}
+
+object DayOfWeek {
+  case object Sunday extends DayOfWeek {
+    override def jodaValue = DateTimeConstants.SUNDAY // 7
+    override def toString = "Sunday"
+  }
+  case object Monday extends DayOfWeek {
+    override def jodaValue = DateTimeConstants.MONDAY // 1
+    override def toString = "Monday"
+  }
+  case object Tuesday extends DayOfWeek {
+    override def jodaValue = DateTimeConstants.TUESDAY
+    override def toString = "Tuesday"
+  }
+  case object Wednesday extends DayOfWeek {
+    override def jodaValue = DateTimeConstants.WEDNESDAY
+    override def toString = "Wednesday"
+  }
+  case object Thursday extends DayOfWeek {
+    override def jodaValue = DateTimeConstants.THURSDAY
+    override def toString = "Thursday"
+  }
+  case object Friday extends DayOfWeek {
+    override def jodaValue = DateTimeConstants.FRIDAY
+    override def toString = "Friday"
+  }
+  case object Saturday extends DayOfWeek {
+    override def jodaValue = DateTimeConstants.SATURDAY
+    override def toString = "Saturday"
+  }
+}
 
 case class MeetingSchedule(
-  dayOfWeek: String,
+  dayOfWeek: DayOfWeek,
   beginningHourUTC: Int,
   beginningMinute: Int = 0
 ) {
@@ -12,11 +47,13 @@ case class MeetingSchedule(
   require(beginningMinute >= 0 && beginningMinute <= 59, s"Invalid beginningMinute[$beginningMinute]")
 
   def upcomingDates(): Seq[DateTime] = {
-    // TODO: Implement actual meeting times
-    Seq(
-      (new DateTime()).plus(MeetingSchedule.OneDay * 7),
-      (new DateTime()).plus(MeetingSchedule.OneDay * 14)
-    )
+    val now = new DateTime(DateTimeZone.UTC).withTime(beginningHourUTC, beginningMinute, 0, 0)
+    val dow = now.weekOfWeekyear().roundFloorCopy().plusDays(dayOfWeek.jodaValue-1).withTime(beginningHourUTC, beginningMinute, 0, 0)
+    if (dow.isBefore(now.plusHours(12))) {
+      Seq(dow.plusWeeks(1), dow.plusWeeks(2))
+    } else {
+      Seq(dow, dow.plusWeeks(1))
+    }
   }
 
 }
@@ -26,7 +63,7 @@ object MeetingSchedule {
   val OneHour = 3600 * 1000l
   val OneDay = OneHour * 24
 
-  val DefaultMeetingSchedule = MeetingSchedule("Thursday", 15)
+  val DefaultMeetingSchedule = MeetingSchedule(DayOfWeek.Thursday, 15)
 
   def findByOrganization(org: Organization): Option[MeetingSchedule] = {
     Some(DefaultMeetingSchedule)
