2d855db0f5e118df3cff360338654278cedd8808
Michael Bryzek <michael@gilt.com>
2014-10-11 08:58:05 +0100

    Expand agenda item tests

--- api/test/controllers/AgendaItemsSpec.scala
+++ api/test/controllers/AgendaItemsSpec.scala
@@ -22,8 +22,30 @@ class AgendaItemsSpec extends BaseSpec {
       )
     )
 
-    println("ITEM: " + item)
-    //val item = createAgendaItem()
+    item.incident.id must be(incident.id)
+    item.task must be(Task.ReviewTeam)
+  }
+
+  "GET /meetings/:meeting_id/agenda_items filters by meeting, agenda item" in new WithServer {
+    val meeting = createMeeting()
+    val item1 = createAgendaItem(meeting = meeting)
+    val item2 = createAgendaItem(meeting = meeting)
+
+    await(client.agendaItems.get(meetingId = meeting.id)).map(_.id).sorted must be(Seq(item1.id, item2.id))
+    await(client.agendaItems.get(meetingId = meeting.id, id = Some(item1.id))).map(_.id) must be(Seq(item1.id))
+    await(client.agendaItems.get(meetingId = meeting.id, id = Some(item2.id))).map(_.id) must be(Seq(item2.id))
+    await(client.agendaItems.get(meetingId = meeting.id, id = Some(-1))).map(_.id) must be(Seq.empty)
+    await(client.agendaItems.get(meetingId = -1, id = Some(item2.id))).map(_.id) must be(Seq.empty)
+  }
+
+  "GET /meetings/:meeting_id/agenda_items paginates" in new WithServer {
+    val meeting = createMeeting()
+    val item1 = createAgendaItem(meeting = meeting)
+    val item2 = createAgendaItem(meeting = meeting)
+
+    await(client.agendaItems.get(meetingId = meeting.id, limit = Some(1))).map(_.id).sorted must be(Seq(item1.id))
+    await(client.agendaItems.get(meetingId = meeting.id, limit = Some(1), offset = Some(1))).map(_.id).sorted must be(Seq(item2.id))
+    await(client.agendaItems.get(meetingId = meeting.id, limit = Some(1), offset = Some(2))).map(_.id).sorted must be(Seq.empty)
   }
 
 }
