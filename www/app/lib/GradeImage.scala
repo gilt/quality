cd5fde909498110ae75c1c0a1aec161ff7225ac2
Michael Bryzek <michael@gilt.com>
2014-10-13 14:47:59 -0400

    Consolidate GradeImage interface to accept a team
     - First step to allowing teams to customize their
       images

--- www/app/lib/GradeImage.scala
+++ www/app/lib/GradeImage.scala
@@ -1,20 +1,24 @@
 package lib
 
+import com.gilt.quality.models.Team
+
 object GradeImage {
 
   val Bad = 0
   val Good = 100
 
-  def imageTag(score: Option[Int]): String = {
+  def imageTag(
+    team: Option[Team],
+    score: Option[Int],
+    size: Int = 25
+  ): String = {
     score match {
       case None => "-"
-      case Some(s) => GradeImage.imageTag(s)
+      case Some(s) => {
+        val filename = if (s <= 50) { "frowny.png" } else { "smiley.png" }
+        s"""<img src="/assets/images/$filename" height="$size" width="$size" />"""
+      }
     }
-  }
-
-  def imageTag(score: Int, size: Int = 25): String = {
-    val filename = if (score <= 50) { "frowny.png" } else { "smiley.png" }
-    s"""<img src="/assets/images/$filename" height="$size" width="$size" />"""
   }
 
 }
