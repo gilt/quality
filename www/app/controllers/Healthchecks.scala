d42f7a78429466995f54ea3d9e54dfebe9837ffd
Michael Bryzek <michael@gilt.com>
2014-10-14 21:32:56 -0400

    Update variable name to match client call

--- www/app/controllers/Healthchecks.scala
+++ www/app/controllers/Healthchecks.scala
@@ -12,7 +12,7 @@ object Healthchecks extends Controller {
    */
   def index() = Action.async { request =>
     for {
-      teams <- client.Api.instance.organizations.get(limit = Some(1))
+      orgs <- client.Api.instance.organizations.get(limit = Some(1))
     } yield {
       Ok("healthy")
     }
