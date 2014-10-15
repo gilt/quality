33c8eefc9f6dce1d4222e200bf9bf077afac32de
Michael Bryzek <michael@gilt.com>
2014-10-15 08:02:12 -0400

    Rename icons => team_icons in db

--- schema/scripts/20141015-075846.sql
+++ schema/scripts/20141015-075846.sql
@@ -1,6 +1,6 @@
-drop table if exists icons;
+drop table if exists team_icons;
 
-create table icons (
+create table team_icons (
   id                      bigserial primary key,
   team_id                 bigint not null references teams,
   name                    text not null
@@ -9,18 +9,18 @@ create table icons (
   url                     text
 );
 
-select schema_evolution_manager.create_basic_audit_data('public', 'icons');
-alter table icons drop column updated_by_guid; -- never updated
+select schema_evolution_manager.create_basic_audit_data('public', 'team_icons');
+alter table team_icons drop column updated_by_guid; -- never updated
 
-create index on icons(team_id);
-create unique index on icons(team_id, name) where deleted_at is null;
+create index on team_icons(team_id);
+create unique index on team_icons(team_id, name) where deleted_at is null;
 
-comment on table icons is '
-  Container to capture any overrides to default icons in use by a
-  given team. We only store icons which are different from the global
+comment on table team_icons is '
+  Container to capture any overrides to default team_icons in use by a
+  given team. We only store team_icons which are different from the global
   defaults for quality.
 ';
 
-comment on column icons.url is '
+comment on column team_icons.url is '
   e.g. smiley or frowny
 ';
