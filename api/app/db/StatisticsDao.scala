37857a14ab398cb434d2d1ad1445a3c644689c86
Michael Bryzek <michael@gilt.com>
2014-10-17 17:03:47 -0400

    Potential bug fix to list more teams on the dashboard, regardless of activity
     - Dashboard should really list all teams, otherwise it appears to be random which teams show up

--- api/app/db/StatisticsDao.scala
+++ api/app/db/StatisticsDao.scala
@@ -20,7 +20,7 @@ object StatisticsDao {
           ceiling(avg(grades.score)) as average_grade 
       from teams 
       join organizations on organizations.deleted_at is null and organizations.id = teams.organization_id and organizations.key = {org_key}
-      left join incidents as all_incidents on all_incidents.team_id = teams.id and all_incidents.deleted_at is null
+      left join incidents as all_incidents on all_incidents.team_id = teams.id and all_incidents.deleted_at is null and all_incidents.created_at >= current_timestamp - ({number_hours} * interval '1 hour')
       left join plans on plans.incident_id = all_incidents.id and plans.deleted_at is null 
       left join grades on grades.plan_id = plans.id and grades.deleted_at is null 
       where teams.deleted_at is null
@@ -35,7 +35,6 @@ object StatisticsDao {
     val sql = Seq(
       Some(BaseQuery.trim),
       teamKey.map { v => "and teams.key = lower(trim({team_key}))" },
-      Some("and all_incidents.created_at >= current_timestamp - ({number_hours} * interval '1 hour')"),
       Some("group by organizations.key, organizations.name, teams.id, teams.key"),
       Some("order by organizations.key, teams.key desc")
     ).flatten.mkString("\n   ")
