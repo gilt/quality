c6354e73517340c9520d54369e47d897167f7727
Michael Bryzek <michael@gilt.com>
2014-10-24 08:54:06 -0400

    UI to add team members

--- www/app/controllers/TeamMembers.scala
+++ www/app/controllers/TeamMembers.scala
@@ -44,7 +44,43 @@ object TeamMembers extends Controller {
   def add(
     org: String,
     key: String
-  ) = TODO
+  ) = TeamAction { implicit request =>
+    Ok(views.html.team_members.add(request.mainTemplate(), request.team, memberForm))
+  }
+
+  def postAdd(
+    org: String,
+    key: String
+  ) = TeamAction.async { implicit request =>
+    val boundForm = memberForm.bindFromRequest
+    boundForm.fold (
+
+      formWithErrors => Future {
+        Ok(views.html.team_members.add(request.mainTemplate(), request.team, formWithErrors))
+      },
+
+      memberForm => {
+        Await.result(Api.instance.users.get(email = Some(memberForm.email)), 1000.millis).headOption match {
+          case None => Future {
+            Ok(views.html.team_members.add(request.mainTemplate(), request.team, boundForm, Some("User not found")))
+          }
+          case Some(user) => {
+            Api.instance.teams.putMembersByOrgAndKeyAndUserGuid(
+              org = request.org.key,
+              key = request.team.key,
+              userGuid = user.guid
+            ).map { member =>
+              Redirect(routes.TeamMembers.show(request.org.key, request.team.key)).flashing("success" -> "Member added")
+            }.recover {
+              case response: com.gilt.quality.error.ErrorsResponse => {
+                Ok(views.html.team_members.add(request.mainTemplate(), request.team, boundForm, Some(response.errors.map(_.message).mkString(", "))))
+              }
+            }
+          }
+        }
+      }
+    )
+  }
 
   def postRemove(
     org: String,
@@ -62,20 +98,29 @@ object TeamMembers extends Controller {
     }
   }
 
-  def postAdd(
+  def postJoin(
     org: String,
-    key: String,
-    userGuid: Option[UUID] = None
+    key: String
   ) = TeamAction.async { request =>
     for {
       member <- Api.instance.teams.putMembersByOrgAndKeyAndUserGuid(
         org = request.org.key,
         key = request.team.key,
-        userGuid = userGuid.getOrElse(request.user.guid)
+        userGuid = request.user.guid
       )
     } yield {
       Redirect(routes.TeamMembers.show(request.org.key, request.team.key)).flashing("success" -> "You are on this team")
     }
   }
+
+  case class MemberForm(
+    email: String
+  )
+
+  private val memberForm = Form(
+    mapping(
+      "email" -> text
+    )(MemberForm.apply)(MemberForm.unapply)
+  )
 
 }
